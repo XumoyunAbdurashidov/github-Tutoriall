590f1f710cc52250fbf6c559ae9f884349638721
Xumoyun <abdurashidovxumoyun4577@gmail.com>
2023-01-09 12:11:14 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// "use strict";
+
+// const channel = prompt("What's is your favourite youtuber");
+// console.log(`https://youtube.com/${channel}/about`);
+
+const x = true;
+const y = false;
+console.log(x || y);
