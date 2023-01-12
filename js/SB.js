53b949cefba71e29b30e05e4952ff7b7c5bcd5b4
Xumoyun <abdurashidovxumoyun4577@gmail.com>
2023-01-12 23:04:20 +0500

    heve a nice day

---
+++ js/SB.js
@@ -0,0 +1,70 @@
+"use strict";
+
+// Amaliyot
+// let numberOffSeries;
+
+// function myFun() {
+//   numberOffSeries = +prompt("Nechta serial ko'rdingiz");
+//   while (
+//     numberOffSeries == "" ||
+//     numberOffSeries == null ||
+//     isNaN(numberOffSeries)
+//   ) {
+//     numberOffSeries = +prompt("Nechta serial ko'rdingiz");
+//   }
+// }
+// myFun();
+
+// const seriesDB = {
+//   count: numberOffSeries,
+//   series: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+
+// function rememberMySeries() {
+//   for (let i = 0; i < 2; i++) {
+//     const a = prompt("oxirgi ko'rgan serialingiz"),
+//       b = prompt("Necha baho berasiz");
+
+//     if (a !== null && b !== null && a != "" && b != "") {
+//       seriesDB.series[a] = b;
+//       console.log("Done");
+//     } else {
+//       i--;
+//       console.log("Eror");
+//     }
+//   }
+// }
+// rememberMySeries();
+
+// function detictLevelSeries() {
+//   if (seriesDB.count < 5) {
+//     console.log("Kam serial ko'ryapsiz");
+//   } else if (seriesDB.count >= 5 && seriesDB.count < 10) {
+//     conslole.log("Siz klassic tomoshabin ekansiz");
+//   } else if (seriesDB.count >= 10) {
+//     console.log("Siz serialchi zvezda ekansiz");
+//   } else {
+//     console.log("Eror");
+//   }
+// }
+// detictLevelSeries();
+
+// function showDB(hidden) {
+//   if (!hidden) {
+//     console.log(seriesDB);
+//   }
+// }
+// showDB(seriesDB.privat);
+
+// function writeGenres() {
+//   for (let i = 0; i <= 2; i++) {
+//     const genre = prompt(`Yaxshi ko'ragan Janiringiz ${i + 1}`);
+//     seriesDB.genres[i] = genre;
+//   }
+// }
+// writeGenres();
+
+// Amaliyot
--- js/index.js
+++
@@ -1,8 +0,0 @@
-// "use strict";
-
-// const channel = prompt("What's is your favourite youtuber");
-// console.log(`https://youtube.com/${channel}/about`);
-
-const x = true;
-const y = false;
-console.log(x || y);
